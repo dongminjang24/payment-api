e04c94d2f8156d187ac3030c42e7c286420e9617
jangdm37 <jangdm37@gmail.com>
2024-08-15 03:31:38 +0900

    deploy ci/cd

---
+++ scripts/before_install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo yum install -y java-21-amazon-corretto
---
+++ scripts/start_application.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/unbuntu/app
+sudo java -jar payment-api-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
