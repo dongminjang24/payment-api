9b162180aa8facf299dd3daf6af8cb89e84a464e
jangdm37 <jangdm37@gmail.com>
2024-08-22 21:59:53 +0900

    sonacube 적용

--- payment-api/src/test/java/com/payment/paymentapi/PaymentApiApplicationTests.java
+++ payment-api/src/test/java/com/payment/paymentapi/PaymentApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.payment.paymentapi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PaymentApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+// package com.payment.paymentapi;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class PaymentApiApplicationTests {
+//
+// 	@Test
+// 	void contextLoads() {
+// 	}
+//
+// }
