2e7d276154dd2d51238e3f9fec9ef8f3cb6b22b3
jangdm37 <jangdm37@gmail.com>
2024-09-04 23:46:57 +0900

    h2 설정 제거 후 로깅6

--- database/src/main/java/com/payment/config/DataSourceConfig.java
+++ database/src/main/java/com/payment/config/DataSourceConfig.java
@@ -2,9 +2,7 @@ package com.payment.config;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -12,33 +10,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 import com.zaxxer.hikari.HikariDataSource;
 
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
+@Slf4j
+@Profile("!dev") // dev 프로파일이 아닌 경우에만 적용
 public class DataSourceConfig {
 
 	@Bean("masterDataSource")
 	@ConfigurationProperties(prefix = "spring.datasource.master")
 	public DataSource masterDataSource() {
-		return DataSourceBuilder.create().build();
+		return DataSourceBuilder.create().type(HikariDataSource.class).build();
 	}
 
 	@Bean("slaveDataSource")
 	@ConfigurationProperties(prefix = "spring.datasource.slave")
 	public DataSource slaveDataSource() {
-		return DataSourceBuilder.create().build();
+		return DataSourceBuilder.create().type(HikariDataSource.class).build();
 	}
-
-
 
 	@Bean
 	@Primary
+	@DependsOn({"masterDataSource", "slaveDataSource"})
 	public DataSource routingDataSource(
 		@Qualifier("masterDataSource") DataSource masterDataSource,
 		@Qualifier("slaveDataSource") DataSource slaveDataSource) {
+
 		RoutingDataSource routingDataSource = new RoutingDataSource();
 
 		Map<Object, Object> targetDataSources = new HashMap<>();
@@ -48,7 +50,9 @@ public class DataSourceConfig {
 		routingDataSource.setTargetDataSources(targetDataSources);
 		routingDataSource.setDefaultTargetDataSource(masterDataSource);
 
-		return routingDataSource;
-	}
+		// 데이터 소스 설정에 대한 로깅 추가
+		log.info("Configured RoutingDataSource with master and slave data sources");
 
+		return new LazyConnectionDataSourceProxy(routingDataSource);
+	}
 }
--- database/src/main/java/com/payment/config/RoutingDataSource.java
+++ database/src/main/java/com/payment/config/RoutingDataSource.java
@@ -1,14 +1,24 @@
 package com.payment.config;
 
 import org.slf4j.MDC;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
+import lombok.extern.slf4j.Slf4j;
 
+@Profile("!dev") // dev 프로파일이 아닌 경우에만 적용
+@Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
+
 	@Override
-	protected Object determineCurrentLookupKey() { // (1)
+	protected Object determineCurrentLookupKey() {
 		String dataSourceType = TransactionSynchronizationManager.isCurrentTransactionReadOnly() ? "slave" : "master";
 		MDC.put("datasource", dataSourceType);
-		return (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) ? "slave" : "master"; //(2)
+		try {
+			return dataSourceType;
+		} finally {
+			MDC.remove("datasource"); // 컨텍스트 정리
+		}
 	}
+
 }
