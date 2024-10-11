826c688b750755419a9cd14607a9267239ab5056
jangdm37 <jangdm37@gmail.com>
2024-10-11 23:25:30 +0900

    kafka concurrency 설정2

--- notification-api/src/main/java/com/payment/notificationapi/batch/NotificationBatchScheduler.java
+++ notification-api/src/main/java/com/payment/notificationapi/batch/NotificationBatchScheduler.java
@@ -22,7 +22,8 @@ public class NotificationBatchScheduler {
 	private final Job notificationJob;
 	private final BatchProperties batchProperties;
 
-	@Scheduled(fixedRate = 300000)
+	// api 호출을 통해 트리거를 하거나, 카프카를 통해서 트리거를 할 수 있음.
+	@Scheduled(fixedRate = 300000) // 스케줄러를 쓰되,스스로 트리거링이 되기 때문에 제어할 수 없음.
 	public void runBatch() throws Exception {
 		JobParameters jobParameters = new JobParametersBuilder()
 			.addString("createdAt", batchProperties.getCreatedAt())
